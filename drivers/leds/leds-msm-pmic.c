5f8bc15b1e29a501a14412724375a2a4e4b1d797
Shkerzy <niksamnoy@gmail.com>
2013-04-25 23:06:05 +0800

    Change for keypad/lcd backlight

--- drivers/leds/leds-msm-pmic.c
+++ drivers/leds/leds-msm-pmic.c
@@ -18,14 +18,26 @@
 #include <linux/leds.h>
 
 #include <mach/pmic.h>
+#include <linux/workqueue.h>
+#include <linux/earlysuspend.h>
 
-#define MAX_KEYPAD_BL_LEVEL	16
+#define MAX_KEYPAD_BL_LEVEL	32
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
+static void msm_pmic_led_early_suspend(struct platform_device *dev,
+		pm_message_t state);
+static void msm_pmic_led_early_resume(struct platform_device *dev);
+static struct early_suspend early_suspend;
+#endif
+
+static struct delayed_work keypad_bl_work;
 static void msm_keypad_bl_led_set(struct led_classdev *led_cdev,
 	enum led_brightness value)
 {
 	int ret;
 
+	printk("msm_keypad_bl_led_set:%d\n",value);
+	ret = pmic_set_led_intensity(LED_LCD, value / MAX_KEYPAD_BL_LEVEL);
 	ret = pmic_set_led_intensity(LED_KEYPAD, value / MAX_KEYPAD_BL_LEVEL);
 	if (ret)
 		dev_err(led_cdev->dev, "can't set keypad backlight\n");
@@ -46,8 +58,20 @@ static int msm_pmic_led_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "unable to register led class driver\n");
 		return rc;
 	}
-	msm_keypad_bl_led_set(&msm_kp_bl_led, LED_OFF);
+#ifdef CONFIG_HAS_EARLYSUSPEND
+        early_suspend.suspend=(void *)msm_pmic_led_early_suspend;
+	early_suspend.resume=(void *)msm_pmic_led_early_resume;
+	register_early_suspend(&early_suspend);
+#endif
+	msm_keypad_bl_led_set(&msm_kp_bl_led, LED_FULL);
+	schedule_delayed_work(&keypad_bl_work, 3 * HZ);
 	return rc;
+}
+
+static void update_kaypad_bl(struct work_struct *work)
+{
+	msm_keypad_bl_led_set(&msm_kp_bl_led, LED_OFF);
+	printk("Close the kaypad back light\n");
 }
 
 static int __devexit msm_pmic_led_remove(struct platform_device *pdev)
@@ -57,31 +81,29 @@ static int __devexit msm_pmic_led_remove(struct platform_device *pdev)
 	return 0;
 }
 
-#ifdef CONFIG_PM
-static int msm_pmic_led_suspend(struct platform_device *dev,
+#ifdef CONFIG_HAS_EARLYSUSPEND
+static void msm_pmic_led_early_suspend(struct platform_device *dev,
 		pm_message_t state)
 {
+	printk("msm_pmic_led_early_suspendxxxxxxxxxxx\n");
 	led_classdev_suspend(&msm_kp_bl_led);
-
-	return 0;
 }
 
-static int msm_pmic_led_resume(struct platform_device *dev)
+static void msm_pmic_led_early_resume(struct platform_device *dev)
 {
+	printk("msm_pmic_led_early_resumeyyyyyyyyyyy\n");
 	led_classdev_resume(&msm_kp_bl_led);
-
-	return 0;
 }
 #else
-#define msm_pmic_led_suspend NULL
-#define msm_pmic_led_resume NULL
+#define msm_pmic_led_early_suspend NULL
+#define msm_pmic_led_early_resume NULL
 #endif
 
 static struct platform_driver msm_pmic_led_driver = {
 	.probe		= msm_pmic_led_probe,
 	.remove		= __devexit_p(msm_pmic_led_remove),
-	.suspend	= msm_pmic_led_suspend,
-	.resume		= msm_pmic_led_resume,
+	.suspend	= NULL,
+	.resume		= NULL,
 	.driver		= {
 		.name	= "pmic-leds",
 		.owner	= THIS_MODULE,
@@ -90,6 +112,8 @@ static struct platform_driver msm_pmic_led_driver = {
 
 static int __init msm_pmic_led_init(void)
 {
+	printk("msm_pmic_led_init\n");
+	INIT_DELAYED_WORK(&keypad_bl_work, update_kaypad_bl);
 	return platform_driver_register(&msm_pmic_led_driver);
 }
 module_init(msm_pmic_led_init);
